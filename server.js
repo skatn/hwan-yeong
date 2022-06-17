08186aadcae2d5721772710ac3cb2f2eea5c03f8
skatn <87296704+skatn@users.noreply.github.com>
2022-06-18 01:29:13 +0900

    헤더 제작, 콘텐트 기본 레이아웃 구축

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 import express from "express";
 import http from "http";
 import ejs from "ejs";
+import path from "path";
+
+const __dirname = path.resolve();
 
 const app = express();
 app.set("port", process.env.PORT || 8000);
 
 app.set("view engine", "ejs");
 app.set("views", "./views");
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.render("index");
